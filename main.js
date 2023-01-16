9ccd750ae500eb3b9d1534095fc6e15fba7f944d
Anna <hannalova1991@gmail.com>
2023-01-16 18:08:21 +0000

    Made decrement function

--- main.js
+++ main.js
@@ -89,7 +89,20 @@ let increment = (id)=>{
 
 
 let decrement = (id)=>{
-  let selectedItem = id;
+let selectedItem = id;
+ let search = basket.find((x)=> x.id === selectedItem.id);
+
+ if(search === undefined){
+  basket.push({
+    id: selectedItem.id,
+    item: 1,
+   });
+ } else {
+  search.item -= 1;
+ };
+
+ 
+ console.log(basket);
  
 };
 
