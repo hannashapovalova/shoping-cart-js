0cb0617f4b8fbd355e2840d36a855515897f5f8e
Anna <hannalova1991@gmail.com>
2023-01-17 18:42:25 +0000

    Code refactoring

--- main.js
+++ main.js
@@ -82,21 +82,24 @@ let increment = (id)=>{
  } else {
   search.item += 1;
  };
- localStorage.setItem("data", JSON.stringify(basket));
+
  update(selectedItem.id);
+ localStorage.setItem("data", JSON.stringify(basket));
 };
 
 let decrement = (id)=>{
-let selectedItem = id;
- let search = basket.find((x)=> x.id === selectedItem.id);
+  let selectedItem = id;
+  let search = basket.find((x)=> x.id === selectedItem.id);
 
- if(search === undefined) return;
- else if(search.item === 0) return;
- else {
-  search.item -= 1;
- };
- localStorage.setItem("data", JSON.stringify(basket));
- update(selectedItem.id);
+  if(search === undefined) return;
+  else if(search.item === 0) return;
+  else {
+   search.item -= 1;
+  };
+
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 
 let update = (id) => {
